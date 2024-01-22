41dfbf87a3ad1bb632d555cba2a9ea97d91a7017
raiaman1234 <120733195+raiaman1234@users.noreply.github.com>
2024-01-22 22:36:36 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import SignUpForm from "./components/signupform/signup";
+
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <SignUpForm />
+    </div>
+  );
+}
+
+export default App;
